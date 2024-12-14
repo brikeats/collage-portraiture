00e797463af5ebae06cf441f31f76103ad46de13
Brian Keating <brikeats@gmail.com>
2024-12-14 15:02:03 -0800

    tweak sigma

--- make_seg_image.py
+++ make_seg_image.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
 from skimage.transform import rescale
+from skimage.morphology import remove_small_holes, remove_small_objects
 from skimage.color import label2rgb
 from skimage.filters import gaussian
 
@@ -13,7 +14,8 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--colors', '-c', default='color-calibration/paper-colors.json', help='JSON file with your papers\' color values')
-    parser.add_argument('--sigma', '-s', default=3, help='Blurring radius. Higher means smoother, simpler shapes')
+    parser.add_argument('--sigma', '-s', default=2, help='Blurring radius. Higher means smoother, simpler shapes')
+    # parser.add_argument('--min-size', type=int, default=50, help='minimim size of objects/holes')
     parser.add_argument('--debug', action='store_true', help='Save intermediate images for debugging')
     parser.add_argument('--out-dir', '-o', default='output-ims')
     parser.add_argument('im_file', help='Your prepared reference photo')
@@ -63,6 +65,8 @@ if __name__ == '__main__':
     seg_im = np.empty(im.shape, np.uint8)
     for (low, hi), rgb in zip(thresh_dict.values(), colors.values()):
         mask = np.logical_and(blurred>=low, blurred<hi)
+        # mask = remove_small_holes(mask, args.min_size)  # usually not a good idea: removes important face details
+        # mask = remove_small_objects(mask, args.min_size)
         value = int(round((low+hi)/2))
         seg_im[mask] = rgb
     seg_fn = path.join(args.out_dir, 'segmentation.png')  # use png; jpeg shows artifacts
